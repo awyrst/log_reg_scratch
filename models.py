dc78ae6a44f49c969ff8b8a8ee2ca6b10977ed19
Alexander Wyrstiuk <awyrstiuk@gmail.com>
2025-05-27 07:23:32 -0400

    Create models.py

---
+++ models.py
@@ -0,0 +1,3 @@
+#Unfortunately, I'm not allowed to share the code for this assignment as part of McGill's academic integrity policy. 
+#However, this is otherwise the file I would have used to store my class definitions and helper functions as described
+#in the readme file
